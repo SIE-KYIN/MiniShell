0fd3548f9d889c2fe3589eed701627de69d94895
Kyujin Lee <kyujlee@c8r1s12.42seoul.kr>
2022-03-18 13:33:48 +0900

    complete UI elements and signal

---
+++ Src/DataStructure/linkedlist2.c
@@ -0,0 +1,24 @@
+#include "../../Inc/minishell.h"
+
+t_list	*create_list(void)
+{
+	t_list	*ret;
+
+	ret = (t_list *)malloc(sizeof(t_list));
+	ret->cnt = 0;
+	ret->top.var = NULL;
+  ret->top.data = NULL;
+  ret->top.prev = NULL;
+  ret->top.next = NULL;
+	return (ret);
+}
+
+void delete_list(t_list *list)
+{
+  if (list)
+  {
+    while (list->cnt > 0)
+      remove_node(list, list->cnt);
+    free(list);
+  }
+}
