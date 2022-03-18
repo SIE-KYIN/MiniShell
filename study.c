98fa8cab5a44fec70e755534a33969450b1ae523
Kyujin Lee <kyujlee@c8r1s12.42seoul.kr>
2022-03-18 13:32:14 +0900

    minishell info

--- study.c
+++
@@ -1,9 +0,0 @@
-#include <dirent.h >
-
-struct dirent{
-	long d_ino; //inode 번호
-	off_t d_off; // offset
-	unsigned short d_reclen; //d_name 길이
-	char d_name[NAME_MAX+1]; //파일 이름
-};
-// 1000, 100 1000~<1100
